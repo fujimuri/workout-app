8852bdd662adc834eab5f2a1fa3aef324d43064c
fujimuri <alexandr.babich@mail.huji.ac.il>
2023-09-09 17:01:42 +0300

    Add field validation and error messages for the single set input fields

--- client/src/components/SingleSet.js
+++ client/src/components/SingleSet.js
@@ -11,16 +11,34 @@ function SingleSet(props) {
     const [currentSetValue, updateSetValue] =
     useState(setInitialValue);
 
+    const [currentErrorMessage, setCurrentErrorMessage] = useState('');
+
+    const [weightError, setWeightError] = useState(false);
+    const [setsError, setSetsError] = useState(false);
+    const [repsError, setRepsError] = useState(false)
+
     function handleWeightChange(e) {
         // update weight
         const setWithUpdatedWeight = currentSetValue;
         // const setWithUpdatedWeight =
         // {...currentSetValue, weight: e.target.value};
-        setWithUpdatedWeight.weight = e.target.value;
+        const newWeight = e.target.value;
+        setWithUpdatedWeight.weight = newWeight;
         updateSetValue(setWithUpdatedWeight);
         // send to fater
         props.handleSetChange(currentSetValue.id, currentSetValue.weight,
         currentSetValue.sets, currentSetValue.reps);
+        // validate input and show error message if needed
+        // TODO update errors variable
+        if (!/^[0-9]*(\.[0-9]+)?$/.test(newWeight)
+        || parseFloat(newWeight) <= 0) {
+            setCurrentErrorMessage('Weight must be a positive number');
+            setWeightError(true);
+        } else {
+            // weight input is ok :)
+            setCurrentErrorMessage('');
+            setWeightError(false);
+        }
     }
 
     function handleSetsChange(e) {
@@ -28,10 +46,21 @@ function SingleSet(props) {
         const setWithUpdatedSets = currentSetValue;
         // const setWithUpdatedSets =
         // {...currentSetValue, sets: e.target.value};
-        setWithUpdatedSets.sets = e.target.value;
+        const newSets = e.target.value;
+        setWithUpdatedSets.sets = newSets;
         updateSetValue(setWithUpdatedSets);
         props.handleSetChange(currentSetValue.id, currentSetValue.weight,
         currentSetValue.sets, currentSetValue.reps);
+        // validate input and show error message if needed
+        // TODO update errors variable
+        if (!/^[1-9]\d*$/.test(newSets)
+        || parseFloat(newSets) <= 0) {
+            setCurrentErrorMessage('Sets must be a whole positive number');
+            setSetsError(true);
+        } else {
+            setCurrentErrorMessage('');
+            setSetsError(false);
+        }
     }
 
     function handleRepsChange(e) {
@@ -39,10 +68,19 @@ function SingleSet(props) {
         const setWithUpdatedReps = currentSetValue;
         // const setWithUpdatedReps =
         // {...currentSetValue, reps: e.target.value};
-        setWithUpdatedReps.reps = e.target.value;
+        const newReps = e.target.value;
+        setWithUpdatedReps.reps = newReps;
         updateSetValue(setWithUpdatedReps);
         props.handleSetChange(currentSetValue.id, currentSetValue.weight,
         currentSetValue.sets, currentSetValue.reps);
+        if (!/^[1-9]\d*$/.test(newReps)
+        || parseFloat(newReps) <= 0) {
+            setCurrentErrorMessage('Reps must be a whole positive number');
+            setRepsError(true);
+        } else {
+            setCurrentErrorMessage('');
+            setRepsError(false);
+        }
     }
 
     function handleSetDeletion(e) {
@@ -51,14 +89,17 @@ function SingleSet(props) {
     }
 
     const editingTemplate = (
-        <div className="single-set-form">
+        <div>
+            <div className="single-set-form">
             <input
             className="input-number"
             type="number"
             step='any'
             placeholder='weight'
             defaultValue={props.weight}
-            onChange={handleWeightChange}>
+            onChange={handleWeightChange}
+            disabled={setsError || repsError }
+            >
             </input>
             <label htmlFor="weight-unit">
                 kg
@@ -69,7 +110,9 @@ function SingleSet(props) {
             step='any'
             placeholder="sets"
             defaultValue={props.sets}
-            onChange={handleSetsChange}>
+            onChange={handleSetsChange}
+            disabled={weightError || repsError }
+            >
             </input>
             <label htmlFor="x">
                 x
@@ -80,7 +123,9 @@ function SingleSet(props) {
             step='any'
             placeholder="reps"
             defaultValue={props.reps}
-            onChange={handleRepsChange}>
+            onChange={handleRepsChange}
+            disabled={weightError || setsError }
+            >
             </input>
             <button
             className="btn delete-set-btn"
@@ -88,6 +133,10 @@ function SingleSet(props) {
             >
                 &#x2715;
             </button>
+            </div>
+            <div className="set-error-messages">
+                {currentErrorMessage}
+            </div>
         </div>
     )
 
